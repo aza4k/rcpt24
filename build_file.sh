b47f6d5d80a4b54152a7ad80240de07c1cab3a5a
Azamat Duysenbaev <128073439+aza4k@users.noreply.github.com>
2025-10-31 21:11:38 +0500

    Update build script for improved commands

--- build_file.sh
+++ build_file.sh
@@ -1,2 +1,6 @@
+#!/bin/bash
+echo "🚀 Build started..."
 pip install -r requirements.txt
-python3.9 manage.py collectstatic
+python manage.py collectstatic --noinput
+python manage.py migrate
+echo "✅ Build completed!"
