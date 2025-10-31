a6d504957cedbb1501d063ddf09189d5e9eb8733
Azamat Duysenbaev <128073439+aza4k@users.noreply.github.com>
2025-10-31 21:41:07 +0500

    Update build_file.sh

--- build_file.sh
+++ build_file.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "🚀 Build started..."
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
-python manage.py migrate
+python3 manage.py collectstatic --noinput
+python3 manage.py migrate
 echo "✅ Build completed!"
